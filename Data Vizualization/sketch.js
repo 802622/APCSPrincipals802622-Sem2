c0708b707db409bbed157966f5ef8e7de1368d36
Nayan Smuek <nayansmuek@nayan.local>
2020-02-12 08:09:10 -0800

    I dislike this

--- Data Vizualization/sketch.js
+++ Data Vizualization/sketch.js
@@ -1,6 +1,7 @@
 //  Nayan Smuek
 //  Project 2/05
 let testTable;
+let points = [];
 
 function preload() {
   testTable = loadTable('assests/biostats.csv', 'csv', 'header');
@@ -11,14 +12,21 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
-
-  // testTable.setNum(1, 'Name', 1);
-  // console.log(testTable.getColumn(0));
 }
 
+function lineMake(){
+  let i = 1;
+  for(;i<18;i++){
+    //let x = (testTable.get(i,1))
+    console.log(i);
+        line(40*i, 100,40*i,10*testTable.get(i, "Age"));
+  }
+}
 
 function draw() {
+  background(220);
+  lineMake()
+  //line(point[1].loc.x,point[1].loc.y,point[2].loc.x,point[2].loc.y)
 }
 
 
