a61a2f2a75b403a7c800cfd515a216dc2f832b9c
Nayan Smuek <nayansmuek@nayan.local>
2020-02-13 08:30:23 -0800

    done

--- Data Vizualization/sketch.js
+++ Data Vizualization/sketch.js
@@ -10,11 +10,11 @@ function preload() {
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  background(244, 127, 129);
+  fill(244, 127, 129);
 
   dropdown = createSelect(); 
-  dropdown.position(150,30); 
+  dropdown.position(150,500); 
 
   dropdown.option("Age"); 
   dropdown.option("Height(in)"); 
@@ -25,24 +25,39 @@ function lineMake(){
   let i = 1;
   for(;i<18;i++){
     variable = dropdown.value();
-      line((20*i)+10, 4*testTable.get(i, variable), (20*i)+10, 800);
+      line((40*i)+20, 800-4*testTable.get(i, variable), (40*i)+20, 750);
+      line((40*i)+30, 800-4*testTable.get(i, variable), (40*i)+30, 750);
+      line((40*i)+20, 800-4*testTable.get(i, variable), (40*i)+30, 800-4*testTable.get(i, variable));
+      line(50,750,750,750)
+      push();
+      textSize(12);
+      translate((40*i)+22,756);
+      rotate(PI/3);
+      text(testTable.get(i, "Name"), 0,0);
+      pop();
   }
+  fill(50);
   if(dropdown.value() == "Age"){
     textSize(16);
-    text('20', 5, 250);
+    text('20 yrs', 5, 700);
+    textSize(48);
+    text("Biostats", 250, 525);
   }
   if(dropdown.value() == "Height(in)"){
     textSize(16);
-    text('60', 5, 350);
+    text('60in', 5, 550);
+    textSize(48);
+    text("Biostats", 250, 450);
   }
   if(dropdown.value() == "Weight(lbs)"){
     textSize(16);
-    text('100', 3, 700);
+    text('100lbs', 3, 400);
+    textSize(48);
+    text("Biostats", 250, 40);
   }
 }
-
 function draw() {
-  background(220);
+  background(244,127,129);
   lineMake()
   //line(point[1].loc.x,point[1].loc.y,point[2].loc.x,point[2].loc.y)
 }
